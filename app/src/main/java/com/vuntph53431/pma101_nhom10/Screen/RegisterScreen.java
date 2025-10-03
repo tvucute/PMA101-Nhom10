f03ad5162f4dad6ae58318d3e8ba9e86c6414317
tvucute <nguyenvublong789@gmail.com>
2025-10-03 21:03:23 +0700

    first commit

---
+++ app/src/main/java/com/vuntph53431/pma101_nhom10/Screen/RegisterScreen.java
@@ -0,0 +1,26 @@
+package com.vuntph53431.pma101_nhom10.Screen;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.vuntph53431.pma101_nhom10.R;
+
+public class RegisterScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
